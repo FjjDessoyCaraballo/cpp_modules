b9bac9f0358795beafd096869ab6e72d78a40834
fjjdessoycaraballo <fdessoycaraballo@gmail.com>
2024-10-02 09:49:06 +0300

    chore: setting up ex01 and ex02

---
+++ cpp02/ex02/inc/Fixed.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*  ROFL:ROFL:ROFL:ROFL 													* */
+/*          _^___      														* */
+/* L     __/   [] \    														* */
+/* LOL===__        \   			MY ROFLCOPTER GOES BRRRRRR					* */
+/* L      \________]  					by fdessoy-							* */
+/*         I   I     			(fdessoy-@student.hive.fi)					* */
+/*        --------/   														* */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+	private:
+		int 				_fpn;
+		static const int 	_il = 8;
+	public:
+		Fixed( void );
+		Fixed( const Fixed &other );
+		Fixed &operator=(const Fixed &other);
+		~Fixed( void );
+
+		// getters
+		int		getRawBits ( void ) const;
+
+		// setters
+		void	setRawBits ( int const raw );
+};
+
+#endif
