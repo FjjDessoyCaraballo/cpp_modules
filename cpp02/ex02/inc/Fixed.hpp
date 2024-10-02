b721e2c347ed68b72f76ccfb6f94c51411e07ebc
fjjdessoycaraballo <fdessoycaraballo@gmail.com>
2024-10-02 14:33:37 +0300

    chore: setting up cpp02/ex02

--- cpp02/ex02/inc/Fixed.hpp
+++ cpp02/ex02/inc/Fixed.hpp
@@ -12,23 +12,30 @@
 #define FIXED_HPP
 
 #include <iostream>
+#include <cmath> // for roundf
 
 class Fixed
 {
 	private:
-		int 				_fpn;
-		static const int 	_il = 8;
+		int 				_fpn; // Fixed Pointer Number
+		static const int 	_il = 8; // Integer Literal
 	public:
 		Fixed( void );
 		Fixed( const Fixed &other );
 		Fixed &operator=(const Fixed &other);
 		~Fixed( void );
+		Fixed ( const int nbr );
+		Fixed ( const float nbr );
 
 		// getters
 		int		getRawBits ( void ) const;
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
 
 		// setters
 		void	setRawBits ( int const raw );
 };
+
+std::ostream	&operator<<(std::ostream &other, Fixed const &nbr);
 
 #endif
