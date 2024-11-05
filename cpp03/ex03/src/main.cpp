3523bdb3919f773dc59978dc3db9f13d12d449e1
fjjdessoycaraballo <fdessoycaraballo@gmail.com>
2024-11-05 12:49:30 +0200

    chore: adding more tests

--- cpp03/ex03/src/main.cpp
+++ cpp03/ex03/src/main.cpp
@@ -27,7 +27,7 @@ int main(void)
 		clap.beRepaired(5);
 		clap.takeDamage(5);
 		clap.takeDamage(5);
-		// clap should be dead ):
+		std::cout << "It was at this moment that clap found out he was dead" << std::endl;
 		clap.beRepaired(5);
 	}
 	
@@ -44,6 +44,25 @@ int main(void)
 		scav.beRepaired(5);
 		scav.takeDamage(5);
 		scav.takeDamage(5);
+	}
+
+	{
+		std::cout << std::endl;
+		std::cout << "look out! Scaver is back! And he brought something!" << std::endl;
+		std::cout << std::endl;
+		ScavTrap scav("Scaver");
+		ClapTrap clap("Clapper");
+
+		std::cout << "Apparently he's going to attack a bag of rotten apples!" << std::endl;
+		scav.attack("big bag of rotten apples");
+		std::cout << "OMG THE ROTTEN APPLES ARE ATTACKING" << std::endl;
+		scav.takeDamage(100);
+		std::cout << "OMG!!11!1! SCAV IS DEAD!" << std::endl;
+		scav.beRepaired(5);
+		clap.attack("big bag of rotten apples");
+		clap.takeDamage(100);
+		clap.beRepaired(5);
+		std::cout << "Seems like this is the end of our mighty Scaver and his friend Clapper" << std::endl;
 	}
 	std::cout << std::endl;
 	std::cout << "### FRAGTRAP ###" << std::endl;
