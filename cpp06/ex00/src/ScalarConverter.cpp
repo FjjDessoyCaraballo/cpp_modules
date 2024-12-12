ac2dd3f8d011eeafa44ff37ee4e1be82b490ec20
fjjdessoycaraballo <fdessoycaraballo@gmail.com>
2024-12-12 13:20:57 +0200

    fix: integers are now printed together with float values

--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -33,23 +33,35 @@ void ScalarConverter::convert( std::string str )
 {
 	try
 	{
+		bool isChar = false;
+		if (!str.empty() && str.back() == 'f')
+			str.pop_back();
 		if (str.length() == 1 && std::isprint(str[0]) && !std::isdigit(str[0]))
 		{
 			char c = str[0];
 			std::cout << "char: " << c << std::endl;
+			isChar = true;
 		}
 		else
 			std::cout << "char: " << "impossible" << std::endl;
 		
 		char *end;
 		errno = 0;
-		long longIntValue = std::strtol(str.c_str(), &end, 10);
-		
-		if (*end == '\0' && errno == 0)
-			std::cout << "int: " << longIntValue << std::endl;
-		else
-			std::cout << "int: impossible" << std::endl;
-		
+		if (isChar == false)
+		{
+			size_t decimalPos = str.find('.');
+
+			long longIntValue = std::strtol(str.c_str(), &end, 10);
+			if (decimalPos != std::string::npos)
+				std::cout << "int: " << str.substr(0, decimalPos) << std::endl;
+			else if (*end == '\0' && errno == 0)
+				std::cout << "int: " << longIntValue << std::endl;
+			else
+				std::cout << "int: impossible" << std::endl;
+		}
+		// else
+		// 	std::cout << "int: impossible" << std::endl;
+
 		errno = 0;
 		float floatValue = std::strtof(str.c_str(), &end);
 
--- cpp06/ex00/src/main.cpp
+++ cpp06/ex00/src/main.cpp
@@ -13,7 +13,11 @@
 int main(int argc, char **argv)
 {
 	if (argc != 2)
+	{
+		std::cout << "Wrong number of arguments, punk" << std::endl;
+		std::cout << "Usage: ./scalar <str>" << std::endl;
 		return (1);
+	}
 	std::string object = argv[1];
 	ScalarConverter::convert(object);
 	return (0);
