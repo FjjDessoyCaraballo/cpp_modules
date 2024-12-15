a823f735025c49190de924d824def62f8383dbf1
fjjdessoycaraballo <fdessoycaraballo@gmail.com>
2024-12-15 13:24:32 +0200

    chore: cleaning up some of the modules

--- cpp01/ex02/src/main.cpp
+++ cpp01/ex02/src/main.cpp
@@ -17,13 +17,18 @@ int main(void)
 	std::string *stringPTR = &str;
 	std::string &stringREF = str;
 
+	// ADDRESSES
 	std::cout << &str << std::endl;
 	std::cout << &stringPTR << std::endl;
 	std::cout << &stringREF << std::endl;
 
+	// STRINGS
 	std::cout << str << std::endl;
 	std::cout << *stringPTR << std::endl;
 	std::cout << stringREF << std::endl;
+
+	// other tests
+	std::cout << stringPTR << std::endl; // prints the address of stringREF
 
 	return (0);
 }
---
+++ cpp06/ex01/inc/ScalarConverter.hpp
@@ -0,0 +1,31 @@
+/* ****************************************************************************/
+/*  ROFL:ROFL:ROFL:ROFL 													  */
+/*          _^___      														  */
+/* L     __/   [] \    														  */
+/* LOL===__        \   			MY ROFLCOPTER GOES BRRRRRR					  */
+/* L      \________]  					by fdessoy-							  */
+/*         I   I     			(fdessoy-@student.hive.fi)					  */
+/*        --------/   														  */
+/* ****************************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+#include <cerrno>
+
+class ScalarConverter
+{
+	private:
+		ScalarConverter();
+		~ScalarConverter();
+	public:
+	// constructor
+		
+	// destructor
+
+	// canonical form
+		ScalarConverter( const ScalarConverter& ref );
+		ScalarConverter& operator=( const ScalarConverter& ref );
+
+	// public methods
+		static void convert( std::string str );
+};
