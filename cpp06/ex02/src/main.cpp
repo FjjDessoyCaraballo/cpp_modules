4da2c62f7bbeadec6c5d1b96e1e6b32565df613d
fjjdessoycaraballo <fdessoycaraballo@gmail.com>
2024-12-18 13:02:41 +0200

    feature: cpp06 done

--- cpp06/ex02/src/main.cpp
+++ cpp06/ex02/src/main.cpp
@@ -12,13 +12,15 @@
 #include "../inc/C.hpp"
 #include "../inc/B.hpp"
 #include "../inc/A.hpp"
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
+#include <random>
 
-static Base* Generate()
+static Base* generate()
 {
-	int a = rand() % 3;
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int> dist(1, 3);
+	int a = dist(gen);
 
 	if (a == 1)
 		return (new A());
@@ -33,17 +35,61 @@ static Base* Generate()
 	}
 }
 
-static void identify(Base *p)
+static void identify(Base* p)
 {
-
+	if (dynamic_cast<C*>(p))
+		std::cout << "It's a C pointer!" << std::endl;
+	else if (dynamic_cast<A*>(p))
+		std::cout << "It's a A pointer!" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "It's a B pointer!" << std::endl;
+	else
+		std::cout << "Stop giving me garbage!" << std::endl;
 }
 
-static void identify(Base &p)
+static void identify(Base& p)
 {
-
+	try {
+		A &a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "T'was a type base reference to A!" << std::endl;
+		return ;
+	} catch (std::exception &e) {
+	}
+	try {
+		B &b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "T'was a type base reference to B!" << std::endl;
+		return ;
+	} catch (std::exception &e) {
+	}
+	try {
+		C &c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "T'was a type base reference to C!" << std::endl;
+		return ;
+	} catch (std::exception &e) {
+	}
 }
 
 int main(void)
 {
+	Base *test1 = generate();
+	Base *test2 = generate();
+
+	std::cout << " ### TESTING FOR THE POINTER IDENTIFICATION ### " << std::endl;
+	if (test1)
+		identify(test1);
+	if (test2)
+		identify(test2);
+
+	std::cout << " ### TESTING FOR THE REFERENCE IDENTIFICATION ### " << std::endl;
+	if (test1)
+		identify(*test1);
+	if (test2)
+		identify(*test2);
+
+	delete test1;
+	delete test2;
 	return (0);
 }
