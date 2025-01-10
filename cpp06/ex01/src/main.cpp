87b7c05597fa2e286de56a67ccac38e4d0e6a59c
fjjdessoycaraballo <fdessoycaraballo@gmail.com>
2025-01-10 14:09:58 +0200

    feature: added interactive testing to cpp06/ex01

--- cpp06/ex01/src/main.cpp
+++ cpp06/ex01/src/main.cpp
@@ -9,18 +9,32 @@
 /* ****************************************************************************/
 
 #include "../inc/Serializer.hpp"
+#include <stdexcept>
 
-int main(void)
+int main(int argc, char **argv)
 {
-	Data data = {42};
-	Data *dataPtr = &data;
+	if (argc != 2)
+	{
+		std::cout << "Usage: <object to be serialized>" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	try {
+		int x = std::stoi(argv[1]);
+		Data data = {x};
+		Data *dataPtr = &data;
+		// Serializer series(42); proof that Serializer cannot be instantiated
 
-	uintptr_t testPtr;
-	testPtr = Serializer::serialize(dataPtr);
-	std::cout << "Serialized: " << testPtr << std::endl;
 
-	Data *retPtr;
-	retPtr = Serializer::deserialize(testPtr);
-	std::cout << "Deserialized: " << retPtr->intValue << std::endl;
+		uintptr_t testPtr;
+		testPtr = Serializer::serialize(dataPtr);
+		std::cout << "Serialized: " << testPtr << std::endl;
+
+		Data *retPtr;
+		retPtr = Serializer::deserialize(testPtr);
+		std::cout << "Deserialized: " << retPtr->intValue << std::endl;
+	} catch (std::exception &e) {
+		std::cerr << "Exception: " << e.what() << std::endl;
+		return (EXIT_FAILURE);
+	}
 	return (0);
 }
